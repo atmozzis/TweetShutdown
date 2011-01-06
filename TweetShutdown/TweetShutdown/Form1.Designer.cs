0d4de579e45235d58c2837afbd04c9a1defd1ff2
@tM <aungthumoe5@gmail.com>
2011-01-06 15:56:45 +0800

    Project Init

---
+++ TweetShutdown/TweetShutdown/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace TweetShutdown
+{
+    partial class frmTweetMyPc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTweetMyPc));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.chkStartAutomatic = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.tmrTweet = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.editSettingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.lblStatus);
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.chkStartAutomatic);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtPassword);
+            this.groupBox1.Controls.Add(this.txtUserName);
+            this.groupBox1.Location = new System.Drawing.Point(9, 9);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(275, 206);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Options";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.ForeColor = System.Drawing.Color.Red;
+            this.lblStatus.Location = new System.Drawing.Point(54, 112);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(6, 141);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(262, 60);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save and Close";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // chkStartAutomatic
+            // 
+            this.chkStartAutomatic.AutoSize = true;
+            this.chkStartAutomatic.Location = new System.Drawing.Point(46, 80);
+            this.chkStartAutomatic.Name = "chkStartAutomatic";
+            this.chkStartAutomatic.Size = new System.Drawing.Size(182, 17);
+            this.chkStartAutomatic.TabIndex = 3;
+            this.chkStartAutomatic.Text = "Start Automatically with Windows";
+            this.chkStartAutomatic.UseVisualStyleBackColor = true;
+            this.chkStartAutomatic.CheckedChanged += new System.EventHandler(this.chkStartAutomatic_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Twitter Password :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Twitter Username :";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(108, 45);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(150, 20);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(108, 19);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(150, 20);
+            this.txtUserName.TabIndex = 2;
+            // 
+            // tmrTweet
+            // 
+            this.tmrTweet.Enabled = true;
+            this.tmrTweet.Interval = 60000;
+            this.tmrTweet.Tick += new System.EventHandler(this.tmrTweet_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editSettingsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(135, 48);
+            // 
+            // editSettingsToolStripMenuItem
+            // 
+            this.editSettingsToolStripMenuItem.Name = "editSettingsToolStripMenuItem";
+            this.editSettingsToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.editSettingsToolStripMenuItem.Text = "Edit &Settings";
+            this.editSettingsToolStripMenuItem.Click += new System.EventHandler(this.editSettingsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            // 
+            // frmTweetMyPc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 224);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmTweetMyPc";
+            this.ShowInTaskbar = false;
+            this.Text = "Tweet My PC";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.CheckBox chkStartAutomatic;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Timer tmrTweet;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem editSettingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ TweetShutdown/TweetShutdown/Form1.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using Yedda;
+
+namespace TweetShutdown
+{
+    public partial class frmTweetMyPc : Form
+    {
+        public frmTweetMyPc()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (txtUserName.Text.Trim() == "")
+            {
+                lblStatus.Text = "* Please Enter Twitter Username *";
+                txtUserName.Focus();
+                return;
+            }
+            else if (txtPassword.Text.Trim() == "")
+            {
+                lblStatus.Text = "* Please Enter Twitter Password *";
+                txtPassword.Focus();
+                return;
+            }
+            else
+            {
+                lblStatus.Text = "";
+            }
+
+            // Check for Valid Username and Password and then Save Settings
+            Twitter objTwitter = new Twitter();
+            XmlDocument Updates = new XmlDocument();
+
+            try // Try logging in
+            {
+                Updates = objTwitter.GetUserTimelineAsXML(txtUserName.Text.Trim(), txtPassword.Text.Trim());
+                Properties.Settings.Default.UserName = txtUserName.Text.Trim();
+                Properties.Settings.Default.Password = txtPassword.Text.Trim();
+                this.WindowState = FormWindowState.Maximized;
+                this.ShowInTaskbar = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to Login to Twitter with the values supplied. Please check your login details. "+ex.Message);
+                txtUserName.Focus();
+                return;
+            }
+        }
+
+        private void chkStartAutomatic_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkStartAutomatic.Checked == true)
+            {
+                Properties.Settings.Default.AutomaticStart = true;
+                RegistryKey regKey = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                regKey.SetValue(Application.ProductName, Application.ExecutablePath);
+                regKey.Close();   
+            }
+            else
+            {
+                Properties.Settings.Default.AutomaticStart = false;
+                RegistryKey regKey = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                regKey.DeleteValue(Application.ProductName);
+                regKey.Close();
+            }
+        }
+
+        private void tmrTweet_Tick(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.UserName == "")
+            {
+                return;
+            }
+            else
+            {
+                Twitter objTwitter = new Twitter();
+                XmlDocument Updates = new XmlDocument();
+
+                try // Try logging in
+                {
+                    Updates = objTwitter.GetUserTimelineAsXML(Properties.Settings.Default.UserName.Trim(),
+                        Properties.Settings.Default.Password.Trim());
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error : Failed to Login to Twitter with the values supplied. Please check your login details.");
+                    return;
+                }
+
+                try
+                {
+                    XmlNode node = Updates.SelectSingleNode("/statuses/status/id");
+                    if(node.InnerText.Trim() != Properties.Settings.Default.LastID.Trim())
+                    {
+                        // Compare the Tweet ID to check for new tweets
+                        Properties.Settings.Default.LastID = node.InnerText.Trim();
+                        node = Updates.SelectSingleNode("/statuses/status/text");
+                        ProcessTweet(node.InnerText.Trim());
+                    }
+                }
+                catch(Exception ex)
+                {
+                    return;
+                }
+            }
+        }
+
+        private void ProcessTweet(String Tweet)
+        {
+            if (Tweet == "shutdown")
+            {
+                System.Diagnostics.Process.Start("shutdown", "-s -f -t 100"); // Shutdown
+            }
+            else if (Tweet == "logoff")
+            {
+                System.Diagnostics.Process.Start("shutdown", "-l -f -t 100"); // Logoff
+            }
+            else if (Tweet == "restart")
+            {
+                System.Diagnostics.Process.Start("shutdown", "-r -f -t 100"); // Restart
+            }
+        }
+
+        private void editSettingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowHideWindows();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ShowHideWindows();
+        }
+
+        private void ShowHideWindows()
+        {
+            tmrTweet.Enabled = false;
+            txtUserName.Text = Properties.Settings.Default.UserName.Trim();
+            txtPassword.Text = Properties.Settings.Default.Password.Trim();
+            if (Properties.Settings.Default.AutomaticStart == true) chkStartAutomatic.Checked = true;
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+        }
+    }
+}
